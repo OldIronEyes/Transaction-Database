7dda81ab48bda7721b9d9d114b6f2469f71b99fe
emilywritescode <emilywritescode@users.noreply.github.com>
2018-11-05 00:46:54 -0500

    adding example functions (get_bars, find_bar, find_beers_less_than)

--- server/BarConsumablePatron/database.py
+++ server/BarConsumablePatron/database.py
@@ -5,9 +5,30 @@ from BarConsumablePatron import config
 
 engine = create_engine(config.database_uri)
 
+# select all bars
 def get_bars():
-	
 	with engine.connect() as con:
-		rs = con.execute("Select Name, License, City, State, Opening, Closing FROM bars;")
+		rs = con.execute("SELECT Name, License, City, State, Opening, Closing FROM Bars;")
 		
+		return [dict(row) for row in rs]
+
+
+# select from Bars given a Bar's name	
+def find_bar(name):
+	with engine.connect() as con:
+		query = sql.text("SELECT Name, License, City, State, Opening, Closing FROM Bars WHERE Name = :name;")
+		
+		rs = con.execute(query, name=name)
+		result = rs.first()
+		if result is None:
+			return None
+		return dict(result)
+
+
+# select all Bar,Beer pairs where Beer's price is less than given max price
+def find_beers_less_than(max_price):
+	with engine.connect() as con:
+		query = sql.text("SELECT * FROM Sells WHERE price < :max_price;")
+		
+		rs = con.execute(query, max_price = max_price)
 		return [dict(row) for row in rs]
