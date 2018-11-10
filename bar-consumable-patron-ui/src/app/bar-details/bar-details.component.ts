3e75b27345d1537ac2d66a4ec30b936f59e643ac
€mily <youallwhylime@gmail.com>
2018-11-09 21:45:28 -0500

    getting barDetails, error logging

--- bar-consumable-patron-ui/src/app/bar-details/bar-details.component.ts
+++ bar-consumable-patron-ui/src/app/bar-details/bar-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BarsService, Bar } from '../bars.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-bar-details',
@@ -10,12 +11,24 @@ import { BarsService, Bar } from '../bars.service';
 export class BarDetailsComponent implements OnInit {
 
         barLicense: string;
+        barDetails: Bar;
 
   constructor(private barService: BarsService, private route: ActivatedRoute) {
           route.paramMap.subscribe((paramMap) => {
                   this.barLicense = paramMap.get('bar');
                   
-                  barService.getBar(this.barLicense)
+                  barService.getBar(this.barLicense).subscribe(
+                          data => 
+                          { this.barDetails = data; },
+                          (error: HttpResponse<any>) => {
+                                  if(error.status === 404){
+                                          alert('Bar not found!');
+                                  } else {
+                                          console.error(error.status + ' : ' + error.body);
+                                          alert('An error occurred!');
+                                  }
+                          }
+                  );
           });
   }
 
