55c52993f8f4cb202339c53b66c47d9e925a51c0
€mily <youallwhylime@gmail.com>
2018-11-10 17:44:49 -0500

    sort those foods by price (ps don't use class float-right << EVIL D:<)

--- bar-consumable-patron-ui/src/app/app.module.ts
+++ bar-consumable-patron-ui/src/app/app.module.ts
@@ -3,23 +3,30 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarsSplashComponent } from './bars-splash/bars-splash.component';
 import { BarDetailsComponent } from './bar-details/bar-details.component';
+import { BeerDetailsComponent } from './beer-details/beer-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarsSplashComponent,
-    BarDetailsComponent
+    BarDetailsComponent,
+    BeerDetailsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    TableModule
+    TableModule,
+    DropdownModule,
+    BrowserAnimationsModule
   ],
   providers: [ HttpClient ],
   bootstrap: [AppComponent]
