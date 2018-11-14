1506cece1ee1d56c55249784545ec3619c210ae8
€mily <youallwhylime@gmail.com>
2018-11-14 12:59:45 -0500

    typo

--- bar-consumable-patron-ui/src/app/beer-details/beer-details.component.ts
+++ bar-consumable-patron-ui/src/app/beer-details/beer-details.component.ts
@@ -81,7 +81,7 @@ export class BeerDetailsComponent implements OnInit {
                                         data=>
                                         { this.transactionsList = data;
                                                 var intervals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
-                                                var amounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+                                                var amounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
                                                 data.forEach(transaction => {
                                                         amounts[transaction.time] = amounts[transaction.time] + transaction.amount;
                                                 });
